427a2b837f3ad533a22895ee8a1fd7139ef4c0f9
Squirrel <squirrelhomebrew@gmail.com>
2025-02-22 15:24:09 -0800

    Update image.js

--- api/image.js
+++ api/image.js
@@ -6,18 +6,15 @@ module.exports = (req, res) => {
   const canvas = createCanvas(600, 600);
   const ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = '#003366'; 
+  ctx.fillStyle = '#003366';
   ctx.fillRect(0, 0, 600, 600);
-
 
   ctx.font = '30px Arial';
   ctx.fillStyle = '#A9D6E5';
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
 
-
   ctx.fillText(message, 300, 300);
-
 
   res.setHeader('Content-Type', 'image/png');
   res.send(canvas.toBuffer('image/png'));
