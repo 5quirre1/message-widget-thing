d8298eb7c3a1b35b2d78929acd82fc9826328025
Squirrel <squirrelhomebrew@gmail.com>
2025-02-22 17:12:24 -0800

    hgggg

--- api/image.js
+++ api/image.js
@@ -1,29 +1,23 @@
 const { createCanvas } = require('@napi-rs/canvas');
 
-
-export default function handler(req, res) {
+module.exports = (req, res) => {
   const message = req.query.message || 'Hello, World!';
 
   const canvas = createCanvas(600, 600);
   const ctx = canvas.getContext('2d');
 
-
   ctx.fillStyle = '#003366';
   ctx.fillRect(0, 0, 600, 600);
-
 
   ctx.font = '30px Arial';
   ctx.fillStyle = '#A9D6E5';
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
-
-
   ctx.fillText(message, 300, 300);
-
 
   const buffer = canvas.toBuffer('image/png');
 
   res.setHeader('Content-Type', 'image/png');
   res.setHeader('Content-Length', buffer.length);
   res.status(200).send(buffer);
-}
+};
